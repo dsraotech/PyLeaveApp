b633bae7721115ceb9c06c781a0c2921e2e7dc43
dsraotech <dsraotech@gmail.com>
2024-05-28 15:38:57 +0530

    changes 2

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 #view.py
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, redirect, request, flash, jsonify, url_for
 from flask_login import login_required, current_user
-from .models import Note
-from . import db
+from .models import Note, User
+from werkzeug.security import generate_password_hash, check_password_hash
 import json
-
+from . import db
+from flask_login import login_required, login_user, current_user, logout_user
+ 
 views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
@@ -32,3 +34,61 @@ def delete_note():
         db.session.commit()
         flash('Note deleted', category='success')
     return jsonify({})
+
+@views.route("/approval", methods=['GET','POST'])
+def leaveapproval():
+    # user = User.query.get(current_user.id)
+    # print("USER : ",type(user))
+    # login_user(user, remember=True)
+    return render_template("approval.html",user=current_user)
+
+@views.route("/reports", methods=['GET','POST'])
+def reports():
+    return render_template("reports.html",user=current_user)
+
+@views.route('/leaveentry', methods=['GET', 'POST'])
+def leaveentry():
+    if request.method == 'POST':
+        user_id = request.form.get('user_id')
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                user.email = email
+                user.first_name = firstname
+                if password1:
+                    if password1 == password2:
+                        user.password = generate_password_hash(password1, method='scrypt')
+                    else:
+                        flash('Confirmed password mismatch', category='error')
+                        return redirect(url_for('views.home'))
+                db.session.commit()
+                flash('User updated successfully', category='success')
+            else:
+                flash('User not found', category='error')
+        else:
+            if User.query.filter_by(email=email).first():
+                flash('Email ID already registered', category='error')
+            elif len(email) < 4:
+                flash('Email must be more than 4 characters', category='error')
+            elif len(firstname) < 3:
+                flash('First name must be more than 3 characters', category='error')
+            elif len(password1) < 3:
+                flash('Password length must be more than 3 characters', category='error')
+            elif password1 != password2:
+                flash('Confirmed password mismatch', category='error')
+            else:
+                newuser = User(email=email, first_name=firstname, password=generate_password_hash(password1, method='scrypt'))
+                db.session.add(newuser)
+                db.session.commit()
+                flash('Signup successful', category='success')
+                login_user(newuser, remember=True)
+                return redirect(url_for('views.home'))
+
+    users = User.query.all()
+    return render_template("signup.html", user=current_user, users=users)
+
